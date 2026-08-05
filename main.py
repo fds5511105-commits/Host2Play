2fc37e31fa30de27a4e2cd6c109f80550b34bf5e
fds5511105-commits <fds5511105@gmail.com>
2026-08-05 19:37:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 
 # 配置区域
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=6666666-6666-6666-6666-66666666666",
+    "https://host2play.gratis/server/renew?i=9fe2b45f-64fc-488d-ad28-6e62b55a91dc",
     # 添加更多链接
 ]
 
